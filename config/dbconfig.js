f4d7ec530c5c08e9daaf0fdec1afd51700ab5a47
Berkelium <53506299+Berkelium-1@users.noreply.github.com>
2020-11-22 14:41:14 +0800

    .
    .

--- config/dbconfig.js
+++ config/dbconfig.js
@@ -4,9 +4,9 @@ module.exports = {
     config: {
         host: 'localhost',
         port: '3306',
-        user: 'root',
-        password: '123456',
-        database: 'my_db'
+        user: 'root', // mysql 用户名
+        password: '123456', // mysql 密码
+        database: 'my_db' // 数据库名
     },
     // 连接数据库, 使用mysql连接池连接方法
     // 连接池对象
@@ -21,6 +21,8 @@ module.exports = {
             conn.query(sql, sqlArr, callBack);
             // 释放连接
             conn.release();
+            // conn.release() 释放连接 不成功时使用以下方法
+            // pool.releaseConnection(conn);
         });
     }
 }
